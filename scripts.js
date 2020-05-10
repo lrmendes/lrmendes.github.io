df368b669809235427991fa3df65440642745eab
lrmendes <lrmen14@gmail.com>
2020-05-10 03:50:14 +0100

    Tablet Screens - Ready

--- scripts.js
+++ scripts.js
@@ -1,5 +1,23 @@
 // Make Cards
 // Color Pallete: https://coolors.co/05668d-028090-00a896-02c39a-f0f3bd
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
 
 const sectionCards = document.querySelector("section.cards");
 
