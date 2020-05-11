3b8b2c63e5ac2342536bad729713ad41a5c45f02
lrmendes <lrmen14@gmail.com>
2020-05-11 12:45:41 +0100

    Projects Tab Changes

--- scripts.js
+++ scripts.js
@@ -25,42 +25,23 @@ const card = document.querySelector("div.card");
 
 const videos = [
   {
-    title: "Formulário Animado com JS puro e CSS Animation | Mayk Brito",
-    duration: "57 min",
-    thumb: "assets/be-the-hero.png",
+    title: "A FullStack project build in a course of Javascript Modern Frameworks.",
+    duration: "Web & Mobile", 
+    thumb: "assets/cards/be-the-hero.png", // 
     video_id: "GykTLqODQuU"
   },
   {
-    title: "Desvendando o CSS Grid na prática | Mayk Brito",
-    duration: "36 min",
-    thumb: "assets/beTheHero.png",
+    title: "E-Commerce System that provides an way for the local stores sell their products and local customers buy them.",
+    duration: "Web & Mobile",
+    thumb: "assets/cards/locdown.png",
     video_id: "HN1UjzRSdBk"
   },
   {
-    title: "Array: Higher Order Functions | Mayk Brito",
-    duration: "54 min",
-    thumb: "https://i.ytimg.com/vi/rAzHvYnQ8DY/hqdefault.jpg",
+    title: "Complete Sales Module for a cooperative Enterprise Resource Planning System.",
+    duration: "Web",
+    thumb: "assets/cards/erpcrm.png",
     video_id: "rAzHvYnQ8DY"
   },
-  {
-    title: "O que é API? REST e RESTful? | Mayk Brito",
-    duration: "33 min",
-    thumb: "https://i.ytimg.com/vi/ghTrp1x_1As/hqdefault.jpg",
-    video_id: "ghTrp1x_1As"
-  },
-  {
-    title: "Desvendando a variável this no Javascript | Mayk Brito",
-    duration: "48 min",
-    thumb: "https://i.ytimg.com/vi/GSqR2i-Pq6o/hqdefault.jpg",
-    video_id: "GSqR2i-Pq6o"
-  },
-  {
-    title:
-      "Como usar Git e Github na prática: Guia para iniciantes | Mayk Brito",
-    duration: "33 min",
-    thumb: "https://i.ytimg.com/vi/2alg7MQ6_sI/hqdefault.jpg",
-    video_id: "2alg7MQ6_sI"
-  }
 ];
 
 videos.map(video => {
